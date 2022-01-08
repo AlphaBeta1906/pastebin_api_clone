77de7e848d3cec99fb9bc2d0ab7595271e346883
fariz <farizi1906@gmail.com>
2022-01-08 10:51:44 +0700

    update config.py

--- api/config.py
+++ api/config.py
@@ -2,9 +2,8 @@
 class Config(object):
     import os
     from datetime import timedelta
-    from dotenv import load_dotenv
+    from dotenv import load_dotenv,dotenv_values
     load_dotenv()
-    
 
     basedir = os.path.abspath(os.path.dirname(__file__))
 
