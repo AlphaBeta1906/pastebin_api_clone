c0e458baa3a0740537e263e6018d5e4a4b2ff162
fariz <farizi1906@gmail.com>
2022-01-08 11:02:46 +0700

    back to previous

--- api/config.py
+++ api/config.py
@@ -1,20 +1,20 @@
+
 class Config(object):
     import os
     from datetime import timedelta
+    from dotenv import load_dotenv,dotenv_values
 
     basedir = os.path.abspath(os.path.dirname(__file__))
+    project_folder = os.path.expanduser('~/pastebin_api_clon')
+    load_dotenv(os.path.join(project_folder, '.env'))
 
     DEBUG = True
     ENV = "development"
-    SECRET_KEY = "ER46324834YGguydgfY89ro4IUYT*d:sd34|dfy#$%"
-    SECURITY_PASSWORD_SALT = "f8wetqw<tq&57g-78b6s6qarrt4Hy2&)12~~/82232e"
-    DEBUG_TB_INTERCEPT_REDIRECTS = False
     SEND_FILE_MAX_AGE_DEFAULT = 0
     CACHE_DEFAULT_TIMEOUT = 3600
     
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:f%40r1Zi1906@localhost:3306/paste"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    print(os.environ.get("DATABASE_URL"))
     SQLALCHEMY_POOL_TIMEOUT = 86400
     SQLALCHEMY_POOL_SIZE = 200
     SQLALCHEMY_POOL_RECYCLE = 100
@@ -25,8 +25,9 @@ class Config(object):
 class ConfigProd(Config):
     ENV = "production"
     DEBUG = False
+    """
     SQLALCHEMY_DATABASE_URI = (
        "mysql+pymysql://pastebinCloneApi:farizi1234@pastebinCloneApi.mysql.pythonanywhere-services.com/pastebinCloneApi$pasteBin"
     )
-    JWT_COOKIE_SECURE = True
+    """
 
