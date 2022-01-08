dcccf5fc1fe5cac7ce4b2a73df8e4d394a7f57e2
fariz <farizi1906@gmail.com>
2022-01-08 09:28:52 +0700

    update config.py

--- api/config.py
+++ api/config.py
@@ -1,3 +1,4 @@
+
 class Config(object):
     import os
     from datetime import timedelta
@@ -6,15 +7,10 @@ class Config(object):
 
     DEBUG = True
     ENV = "development"
-    SECRET_KEY = "ER46324834YGguydgfY89ro4IUYT*d:sd34|dfy#$%"
-    SECURITY_PASSWORD_SALT = "f8wetqw<tq&57g-78b6s6qarrt4Hy2&)12~~/82232e"
-    DEBUG_TB_INTERCEPT_REDIRECTS = False
     SEND_FILE_MAX_AGE_DEFAULT = 0
     CACHE_DEFAULT_TIMEOUT = 3600
     
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:f%40r1Zi1906@localhost:3306/paste"
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_POOL_TIMEOUT = 86400
     SQLALCHEMY_POOL_SIZE = 200
     SQLALCHEMY_POOL_RECYCLE = 100
@@ -25,7 +21,9 @@ class Config(object):
 class ConfigProd(Config):
     ENV = "production"
     DEBUG = False
+    """
     SQLALCHEMY_DATABASE_URI = (
        "mysql+pymysql://pastebinCloneApi:farizi1234@pastebinCloneApi.mysql.pythonanywhere-services.com/pastebinCloneApi$pasteBin"
     )
-    JWT_COOKIE_SECURE = True
+    """
+
