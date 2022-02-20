12e40d92ea3cea97de20621c3ac2593109b61721
fariz <farizi1906@gmail.com>
2022-02-20 20:39:25 +0700

    prevent untitled paste to appear

--- api/model.py
+++ api/model.py
@@ -32,7 +32,7 @@ class Paste(db.Model):
         db.session.commit()
     def get_paste_desc_paged(self,offset,_language=None):
         pastes = (self.query
-                     .filter((Paste.unique_id != "") | (Paste.title != ""))
+                     .filter(or_(Paste.unique_id != "",Paste.title != ""))
                      .order_by(Paste.id.desc())
                      .offset(offset)
                      .limit(10)
@@ -40,7 +40,7 @@ class Paste(db.Model):
                )
         if _language:
             pastes = (self.query
-                     .filter((Paste.unique_id != "") | (Paste.title != ""))
+                     .filter(or_(Paste.unique_id != "",Paste.title != ""))
                      .filter_by(language=_language)
                      .order_by(Paste.id.desc())
                      .offset(offset)
