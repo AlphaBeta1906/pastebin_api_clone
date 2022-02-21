2c77fd7a850e39ea1d8689bc54a9192daa270a45
fariz <farizi1906@gmail.com>
2022-02-21 12:47:24 +0700

    update test and add test db

--- test_api.py
+++ test_api.py
@@ -1,35 +1,24 @@
-import requests
+import pytest
+from api import run,db
+from api.config import ConfigTest
 
+app = run()
+@pytest.fixture
+def client():
+    app.config.from_object(ConfigTest)
+    
+    with app.app_context():
+        from api.model import Paste
+        db.create_all()
+        db.session.commit()
+    
+    return app.test_client()
 
-def test_post_route():
-    code_title = "test"
-    code = """
-    class Codes(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        unique_id = db.Column(db.String(1000), nullable=False)
-        code_title = db.Column(db.String(1000), nullable=False)
-        code = db.Column(db.String(10000), nullable=False)
-        languange = db.Column(db.String(1000), nullable=False)
-        date_added = db.Column(db.DateTime(), default=datetime.today(), nullable=False)
-    """
-    languange = "python"
-    req = requests.post(
-        f"http://127.0.0.1:5000/add_code?code_title={code_title}&code={code}&languange={languange}"
-    )
+def testing_route(client):
+    response = client.get("api/v1/test")
+    assert response.status_code == 200
 
-    print(req.status_code)
-    print(req.json())
-    print(req.url)
-    assert req.status_code == 200
-    assert req.json()["message"] != "error"
-
-
-def test_get_route():
-    import json
-
-    req = requests.get("http://127.0.0.1:5000/get_code/fr5m")
-    print(json.dumps(req.json(), indent=4))
-    assert req.status_code == 200
-
-
-test_get_route()
+def test_post(client):
+    response = client.post("api/v1/paste",json= {"title": "testing", "code": "print('hello wordl')","language": "python"})
+    print(response)
+    assert response.status_code == 200    
